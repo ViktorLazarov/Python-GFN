129640356cc3f26367895c583e3a948f67a4340c
Viktor Lazarov <viktorlazarov@abv.bg>
2024-01-14 20:06:34 +0100

    asd

---
+++ lv8_Hangman_Step1.py
@@ -0,0 +1,106 @@
+#   _  _                                
+#  | || |__ _ _ _  __ _ _ __  __ _ _ _  
+#  | __ / _` | ' \/ _` | '  \/ _` | ' \ 
+#  |_||_\__,_|_||_\__, |_|_|_\__,_|_||_|
+#                 |___/                 
+import random
+with open('wordlist 1.txt', 'r') as file:
+    word_list2 = file.read()
+    
+import art
+stages = art.stages
+
+# stages = ['''
+#   +---+
+#   |   |
+#   O   |
+#  /|\  |
+#  / \  |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#  /|\  |
+#  /    |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#  /|\  |
+#       |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#  /|   |
+#       |
+#       |
+# =========''', '''
+#   +---+
+#   |   |
+#   O   |
+#   |   |
+#       |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#       |
+#       |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#       |
+#       |
+#       |
+#       |
+# =========
+# ''']
+# Step 1
+
+word_list = ["Dragonball", "JavaScript", "Python", "Programmieren"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = word_list2[random.randint(0,len(word_list2)-1)]
+chosen_word_list = list(chosen_word.lower())
+print(chosen_word_list)
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# userGuess = input("Make a guess: ").lower()
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+lives = 7
+wordIsFound = False
+while True:
+    userGuess = input("Make a guess: ").lower()
+    if userGuess in chosen_word_list:
+        print("Correct")
+        chosen_word_list[chosen_word_list.index(userGuess)] = ' '
+        print(chosen_word_list)
+        for letter in chosen_word_list:
+            if letter == ' ':
+                wordIsFound = True
+            else:
+                wordIsFound = False
+    else:
+        print("Wrong, try again")
+        print(stages[lives-1])
+        lives -= 1
+        
+    if wordIsFound:
+        print("You found all the letters.")
+        break
+    if lives == 0:
+        print("No more lives.")
+        break
+    
+print("Thx for playing!")
+
