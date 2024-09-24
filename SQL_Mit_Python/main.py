eb8fd11553a6d1974a9e9bbbaebba142b2d13782
Viktor Lazarov <viktorlazarov@abv.bg>
2024-09-24 09:16:18 +0200

    create test DB

--- SQL_Mit_Python/main.py
+++ SQL_Mit_Python/main.py
@@ -1,3 +1,15 @@
 # SQL mit Python
 import sqlite3
 
+# connects to a DB, if it doesn't exists, first it will create it
+conn = sqlite3.connect("test.db")
+
+
+
+
+
+
+
+
+# close connection
+conn.close()
